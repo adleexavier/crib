90df0191ecc4d9f3d6b9f4ec06522c7b4423a0a3
Adlee <adlee@qburst.com>
2023-12-19 10:46:13 +0530

    Completed the crib

--- src/Components/crib.ts
+++ src/Components/crib.ts
@@ -1,128 +1,123 @@
 /******************************* */
 import {
-    Scene,
-    Engine,
-    SceneLoader,
-    HemisphericLight,
-    Vector3,
-    UniversalCamera,
-    Mesh,
-    PointLight,
-    GlowLayer,
-  } from "@babylonjs/core";
-  import "@babylonjs/loaders";
- 
-  import {
-    setScene,
-    setNavManager,
-    setCamManager,
-  } from "./metaGlobal";
+  Scene,
+  Engine,
+  SceneLoader,
+  HemisphericLight,
+  Vector3,
+  UniversalCamera,
+  Mesh,
+  PointLight,
+  GlowLayer,
+  DirectionalLight,
+  HDRCubeTexture,
+  Color4,
+  CubeTexture,
+} from "@babylonjs/core";
+import "@babylonjs/loaders";
+
+import { setScene, setNavManager, setCamManager } from "./metaGlobal";
 
 import { Music } from "./metaComponents/music";
 import { NavMeshManager } from "./metaComponents/navMesh";
 import { CameraManager } from "./metaComponents/camera";
-  export class myCrib {
-    scene: Scene;
-    engine: Engine;
-    camera!: UniversalCamera;
-    cameraManager = {} as CameraManager;
-    navMeshManager = {} as NavMeshManager;
-  
-    //*********CONSTRUCTOR************/
-    constructor(
-      private canvas: HTMLCanvasElement,
+export class myCrib {
+  scene: Scene;
+  engine: Engine;
+  camera!: UniversalCamera;
+  cameraManager = {} as CameraManager;
+  navMeshManager = {} as NavMeshManager;
 
-    ) {
-      this.engine = new Engine(this.canvas, true)
-      this.scene = this.createScene();
-      this.scene.gravity = new Vector3(0,-9.81/30,0);
-      this.scene.collisionsEnabled = true;
-      setScene(this.scene);
-      this.cloneConstructor();
-    }
-  
-    async cloneConstructor() {
-      await this.createEnvironment();
-      /********************************** */
-      this.cameraManager = new CameraManager(this.scene);
-      setCamManager(this.cameraManager);
-  
-      this.engine.runRenderLoop(() => {
-        this.scene.render();
-      });
-    }
-    createScene(): Scene {
-      const scene = new Scene(this.engine);
-      // Add lights
-      const hemiLight = new HemisphericLight(
-        "hemiLight",
-        new Vector3(0, 1, 0),
-        this.scene
-      );
-      hemiLight.intensity = 1;
-  
-      this.canvas.addEventListener("contextmenu", (e) => {
-        this.engine.enterPointerlock();
-      });
-      this.canvas.addEventListener("dblclick", (e) => {
-        this.engine.exitPointerlock();
-      });
-  
-
-      const cribMusic = new Music(
-        "crib",
-        "/Audio/music.mp3",
-        this.scene
-      );
-      /*
-       * set music configuration
-       */
-      cribMusic.soundConfig({
-        position: new Vector3(42, 2,44.4),
-        distanceModal: "exponential",
-        coneAngle: Math.PI / 2,
-        volume: 0.1,
-      });
-  
-      return scene;
-    }
-  
-    async createEnvironment(): Promise<void> {
-      this.engine.displayLoadingUI();
-  
-      const { meshes } = await SceneLoader.ImportMeshAsync(
-        "",
-        "/models/",
-        `crib2.glb`,
-        this.scene
-      );
-      meshes.map(mesh=>{
-       if (mesh.name.startsWith("floor"))
-       mesh.checkCollisions = true
-      })
-      const { meshes : stars } = await SceneLoader.ImportMeshAsync(
-        "",
-        "/models/",
-        `star.glb`,
-        this.scene
-      );
-      stars[0].position = new Vector3(42, 15,44.4)
-      const pointLight = new PointLight('starlight',new Vector3(42, 15,44.4),this.scene)
-      const gl = new GlowLayer("glow", this.scene);
-      gl.intensity = 0.5;
-      pointLight.intensity = 20
-   
-      this.engine.hideLoadingUI();
-    }
-
-  
-  
-  
-    terminateMetaverse() {
-      this.engine.dispose();
-      this.scene.dispose();
-      this.engine = {} as Engine;
-      this.scene = {} as Scene;
-    }
+  //*********CONSTRUCTOR************/
+  constructor(private canvas: HTMLCanvasElement) {
+    this.engine = new Engine(this.canvas, true);
+    this.scene = this.createScene();
+    this.scene.gravity = new Vector3(0, -9.81 / 30, 0);
+    this.scene.collisionsEnabled = true;
+    setScene(this.scene);
+    this.cloneConstructor();
   }
-  
+
+  async cloneConstructor() {
+    await this.createEnvironment();
+    /********************************** */
+    this.cameraManager = new CameraManager(this.scene);
+    setCamManager(this.cameraManager);
+
+    this.engine.runRenderLoop(() => {
+      this.scene.render();
+    });
+  }
+  createScene(): Scene {
+    const scene = new Scene(this.engine);
+    // Add lights
+    const hemiLight = new HemisphericLight(
+      "hemiLight",
+      new Vector3(0, 1, 0),
+      this.scene
+    );
+    hemiLight.intensity = 1;
+
+    this.canvas.addEventListener("contextmenu", (e) => {
+      this.engine.enterPointerlock();
+    });
+    this.canvas.addEventListener("dblclick", (e) => {
+      this.engine.exitPointerlock();
+    });
+
+    const cribMusic = new Music("crib", "/Audio/music.mp3", this.scene);
+    /*
+     * set music configuration
+     */
+    cribMusic.soundConfig({
+      position: new Vector3(42, 2, 44.4),
+      distanceModal: "exponential",
+      coneAngle: Math.PI / 2,
+      volume: 0.1,
+    });
+
+    return scene;
+  }
+
+  async createEnvironment(): Promise<void> {
+    this.engine.displayLoadingUI();
+
+    const { meshes } = await SceneLoader.ImportMeshAsync(
+      "",
+      "/models/",
+      `crib2.glb`,
+      this.scene
+    );
+    meshes.map((mesh) => {
+      if (mesh.name.startsWith("floor")) mesh.checkCollisions = true;
+    });
+    const { meshes: stars } = await SceneLoader.ImportMeshAsync(
+      "",
+      "/models/",
+      `star.glb`,
+      this.scene
+    );
+    stars[0].position = new Vector3(42, 15, 44.4);
+    const pointLight = new DirectionalLight(
+      "starlight",
+      new Vector3(42, 0, 44.4),
+      this.scene
+    );
+    pointLight.position = new Vector3(42, 20, 44.4);
+    const gl = new GlowLayer("glow", this.scene);
+    gl.intensity = 0.5;
+    pointLight.intensity = 1.5;
+    pointLight.radius = 10;
+    /*************************** */
+    this.scene.clearColor = new Color4(0, 0, 0, 1);
+
+    this.engine.hideLoadingUI();
+  }
+
+  terminateMetaverse() {
+    this.engine.dispose();
+    this.scene.dispose();
+    this.engine = {} as Engine;
+    this.scene = {} as Scene;
+  }
+}
--- src/Components/metaComponents/camera.ts
+++ src/Components/metaComponents/camera.ts
@@ -1,44 +1,42 @@
 import {
-    Scene,
-    UniversalCamera,
-    Vector3,
-    TransformNode,
-    AbstractMesh,
-  } from "@babylonjs/core";
-  
-  export class CameraManager {
-    camera = {} as UniversalCamera;
-    scene = {} as Scene;
-    constructor(scene: Scene) {
-      this.scene = scene;
-      this.createCam();
-    }
-    createCam() {
-      this.camera = new UniversalCamera("cam", new Vector3(-7.25, 4.02, -16.3), this.scene);
-      this.camera.angularSensibility = 3200;
-      this.camera.minZ = 0.3;
-      this.camera.attachControl();
-      // this.camera.inputs.attached.mouse.detachControl();
-      this.camera.attachControl()
-      this.camera.speed = 0.8
-      this.camera.applyGravity = true
-      this.camera.checkCollisions = true
-      this.camera.ellipsoid = new Vector3(1,2,1)
-      // this.camera.inputs.attached.keyboard.detachControl();
-      this.scene.activeCamera = this.camera;
-      this.scene.onBeforeRenderObservable.add((()=>{
-                 console.log(this.camera.position);
-      }))
-    }
-    connectWithUser(transformNode: TransformNode) {
-      this.camera.parent = transformNode;
-      this.camera.position.y = 1;
-      this.camera.rotation.y = Math.PI;
-    }
-    lookAt(mesh: AbstractMesh | undefined) {
-      if (mesh) this.camera.setTarget(mesh.absolutePosition);
-    }
+  Scene,
+  UniversalCamera,
+  Vector3,
+  TransformNode,
+  AbstractMesh,
+} from "@babylonjs/core";
 
-
+export class CameraManager {
+  camera = {} as UniversalCamera;
+  scene = {} as Scene;
+  constructor(scene: Scene) {
+    this.scene = scene;
+    this.createCam();
   }
-  
+  createCam() {
+    this.camera = new UniversalCamera(
+      "cam",
+      new Vector3(-7.25, 4.02, -16.3),
+      this.scene
+    );
+    this.camera.angularSensibility = 3200;
+    this.camera.minZ = 0.3;
+    this.camera.attachControl();
+    // this.camera.inputs.attached.mouse.detachControl();
+    this.camera.attachControl();
+    this.camera.speed = 0.8;
+    this.camera.applyGravity = true;
+    this.camera.checkCollisions = true;
+    this.camera.ellipsoid = new Vector3(1, 2, 1);
+    // this.camera.inputs.attached.keyboard.detachControl();
+    this.scene.activeCamera = this.camera;
+  }
+  connectWithUser(transformNode: TransformNode) {
+    this.camera.parent = transformNode;
+    this.camera.position.y = 1;
+    this.camera.rotation.y = Math.PI;
+  }
+  lookAt(mesh: AbstractMesh | undefined) {
+    if (mesh) this.camera.setTarget(mesh.absolutePosition);
+  }
+}
